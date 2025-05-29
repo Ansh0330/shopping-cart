d9aa18331dd3f2eb8ed2d5b6be692330c517e13a
Ansh Kotnala <meanshbhardwaj@gmail.com>
2025-05-29 07:38:20 +0530

    change loader css

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div className='spinner'></div>
+    
+    <div className='w-full h-screen flex items-center justify-center'>
+      <div className='spinner'></div>
+    </div>
   )
 }
 
